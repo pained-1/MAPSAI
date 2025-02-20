8f08582652f68eeb2f1da21d09ff65d774226f4a
Georg <georgtsvetkof@gmail.com>
2025-02-20 20:04:21 +0300

    geo

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-import os
-import sys
-from setting import *
-import pygame
+from io import BytesIO
 import requests
+from PIL import Image
 
-server_address = 'https://static-maps.yandex.ru/v1?'
-api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll_spn = 'll=37.530887,55.703118&spn=1,1'
-# Готовим запрос.
+search_api_server = "https://search-maps.yandex.ru/v1"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-map_request = f"{server_address}{ll_spn}&apikey={api_key}"
-response = requests.get(map_request)
+address_ll = "37.588392,55.734036"
 
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"}
+
+response = requests.get(search_api_server, params=search_params)
 if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
-# Инициализируем pygame
-
-pygame.init()
-screen = pygame.display.set_mode((WIGHT, HEIGHT))
-# Рисуем картинку, загружаемую из только что созданного файла.
-a = pygame.image.load(map_file)
-pygame.transform.scale(a,(WIGHT, HEIGHT))
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
     pass
-pygame.quit()
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+
+json_response = response.json()
+
+organization = json_response["features"][0]
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+point = organization["geometry"]["coordinates"]
+org_point = f"{point[0]},{point[1]}"
+delta = "0.005"
+apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+map_params = {
+    "ll": address_ll,
+    "spn": ",".join([delta, delta]),
+    "pt": f"{org_point},pm2dgl",
+    "l": "map"
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+print(response)
+im = BytesIO(response.content)
+opened_image = Image.open(im)
+opened_image.show()
